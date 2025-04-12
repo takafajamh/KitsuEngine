27522810b814e9925da79f391cd57958f5541d6b
Rafal F <rafalfuchs2004@gmail.com>
2025-04-12 14:05:28 +0200

    Added dependencies and setup

---
+++ KitsuEngine/KitsuEngine.cpp
@@ -0,0 +1,11 @@
+﻿// KitsuEngine.cpp : Definiuje funkcje biblioteki statycznej.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <iostream>
+
+void ThisIsATest()
+{
+	std::cout << "TEST" << std::endl;
+}
