68728d140ed2377535ab7d6afb481847e5922e14
Rafal F <rafalfuchs2004@gmail.com>
2025-06-22 16:16:45 +0200

    FRect Intersect helper

--- KitsuEngine/Globals.hpp
+++ KitsuEngine/Globals.hpp
@@ -6,3 +6,10 @@
 inline SDL_Window* window = nullptr;
 inline SDL_Renderer* renderer = nullptr;
 inline float dt = 0;
+
+
+inline bool SDL_FRectIntersects(const SDL_FRect& a, const SDL_FRect& b)
+{
+	return !(a.x + a.w <= b.x || b.x + b.w <= a.x ||
+		a.y + a.h <= b.y || b.y + b.h <= a.y);
+}
